0eb9d872b55a3e479705add735e368293cd535ce
moonyl <moonysl@hanmail.net>
2019-02-13 06:18:55 +0900

    run to 26

---
+++ SoundexTest.cpp
@@ -0,0 +1,39 @@
+#include <gmock/gmock.h>
+
+#include "Soundex.h"
+
+using namespace testing;
+
+class SoundexEncoding : public Test {
+public:
+	Soundex soundex;
+};
+
+TEST_F(SoundexEncoding, RetainsSoleLetterOfOneLetterWord) {	
+
+	ASSERT_THAT(soundex.encode("A"), Eq("A000"));
+}
+
+TEST_F(SoundexEncoding, PadsWithZerosToEnsureThreeDigits) {
+
+	ASSERT_THAT(soundex.encode("I"), Eq("I000"));
+}
+
+TEST_F(SoundexEncoding, ReplacesConstantsWithAppropriateDigits) {
+	//ASSERT_THAT(soundex.encode("Ab"), Eq("A100"));
+	//ASSERT_THAT(soundex.encode("Ac"), Eq("A200"));
+	//ASSERT_THAT(soundex.encode("Ad"), Eq("A300"));
+	ASSERT_THAT(soundex.encode("Ax"), Eq("A200"));
+}
+
+TEST_F(SoundexEncoding, IgnoresNonAlphabetics) {
+	ASSERT_THAT(soundex.encode("A#"), Eq("A000"));
+}
+
+TEST_F(SoundexEncoding, ReplacesMultipleConstantsWithDigits) {
+	ASSERT_THAT(soundex.encode("Acdl"), Eq("A234"));
+}
+
+TEST_F(SoundexEncoding, LimitsLengthToFourCharacters) {
+	ASSERT_THAT(soundex.encode("Dcdlb").length(), Eq(4u));
+}
