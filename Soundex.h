3975eaf1423dd2f28b59a3843d629f609ef70a6b
moonyl <moonysl@hanmail.net>
2019-02-14 07:22:21 +0900

    to 36

--- Soundex.h
+++ Soundex.h
@@ -3,12 +3,14 @@
 
 #include <string>
 #include <unordered_map>
+#include <cctype>
 
 class Soundex {
 public:
 	static const size_t MaxCodeLength{ 4 };
+
 	std::string encode(const std::string& word) const {
-		return zeroPad(head(word) + encodedDigits(tail(word)));
+		return zeroPad(upperFront(head(word)) + tail(encodedDigits(word)));
 	}
 	   
 private:
@@ -20,16 +22,34 @@ private:
 		return word.substr(1);
 	}
 
+	const std::string NotADigit{ "*" };
+
 	std::string encodedDigits(const std::string & word) const
 	{
 		std::string encoding;
-		for (auto letter : word) {
-			if (encoding.length() == MaxCodeLength - 1) break;
-			encoding += encodedDigit(letter);
+		encoding += encodedDigit(word.front());
+
+		for (auto letter : tail(word)) {
+			if (isComplete(encoding)) break;
+
+			auto digit = encodedDigit(letter);
+			if (digit != NotADigit && digit != lastDigit(encoding))
+				encoding += digit;
 		}
 		return encoding;
 	}
 
+	std::string lastDigit(const std::string& encoding) const {
+		if (encoding.empty()) return NotADigit;
+		return std::string(1, encoding.back());
+	}
+
+	bool isComplete(const std::string &encoding) const
+	{
+		return encoding.length() == MaxCodeLength;
+	}
+
+public:
 	std::string encodedDigit(char letter) const {
 		const std::unordered_map<char, std::string> encodings{
 			{'b', "1"}, {'f', "1"}, {'p', "1"}, {'v', "1"},
@@ -41,13 +61,23 @@ private:
 			{'r', "6"}
 		};
 		
-		auto it = encodings.find(letter);
-		return it == encodings.end() ? "" : it->second;
+		auto it = encodings.find(lower(letter));
+		return it == encodings.end() ? NotADigit : it->second;
 	}
 	   
+private:
+	char lower(char c) const {
+		return std::tolower(static_cast<unsigned char>(c));
+	}
+
 	std::string zeroPad(const std::string & word) const {
 		auto zerosNeeded = MaxCodeLength - word.length();
 		return word + std::string(zerosNeeded, '0');
+	}
+
+	std::string upperFront(const std::string& string) const {
+		return std::string(1, 
+			std::toupper(static_cast<unsigned char>(string.front())));
 	}
 };
 
--- SoundexTest.cpp
+++ SoundexTest.cpp
@@ -37,3 +37,27 @@ TEST_F(SoundexEncoding, ReplacesMultipleConstantsWithDigits) {
 TEST_F(SoundexEncoding, LimitsLengthToFourCharacters) {
 	ASSERT_THAT(soundex.encode("Dcdlb").length(), Eq(4u));
 }
+
+TEST_F(SoundexEncoding, IgnoresVowelLikeLetters) {
+	ASSERT_THAT(soundex.encode("BaAeEiIoOuUhHyYcdl"), Eq("B234"));
+}
+
+TEST_F(SoundexEncoding, CombinesDuplicateEncodings) {
+	ASSERT_THAT(soundex.encodedDigit('b'), Eq(soundex.encodedDigit('f')));
+	ASSERT_THAT(soundex.encodedDigit('c'), Eq(soundex.encodedDigit('g')));
+	ASSERT_THAT(soundex.encodedDigit('d'), Eq(soundex.encodedDigit('t')));
+
+	ASSERT_THAT(soundex.encode("Abfcgdt"), Eq("A123"));
+}
+
+TEST_F(SoundexEncoding, UppercasesFirstLetter) {
+	ASSERT_THAT(soundex.encode("abcd"), StartsWith("A"));
+}
+
+TEST_F(SoundexEncoding, IgnoresCaseWhenEncodingConsonants) {
+	ASSERT_THAT(soundex.encode("BCDL"), Eq(soundex.encode("Bcdl")));
+}
+
+TEST_F(SoundexEncoding, CombinesDuplicateCodesWhen2ndLetterDuplicates1st) {
+	ASSERT_THAT(soundex.encode("Bbcd"), Eq("B230"));
+}
